92247f160e3c509a9e25d155510420fc13971a88
Gloria <g.calabrese@itsrizzoli.it>
2019-01-09 15:38:11 +0100

    progetto

---
+++ app-rendere/src/main/java/project/Terapia.java
@@ -0,0 +1,53 @@
+package project;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Terapia implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue (strategy=GenerationType.IDENTITY)
+	@ManyToOne
+	private Utente utente;
+	
+	@OneToMany(mappedBy="terapia")
+	private List<Farmaco> farmaci;
+	
+	@ManyToOne
+	private Allarme allarmi;
+	
+	public Terapia() {
+		super();
+	}
+
+	public Utente getUtente() {
+		return utente;
+	}
+
+	public void setUtente(Utente utenti) {
+		this.utente = utenti;
+	}
+
+	public List<Farmaco> getFarmaci() {
+		return farmaci;
+	}
+
+	public void setFarmaci(List<Farmaco> farmaci) {
+		this.farmaci = farmaci;
+	}
+
+	public static long getSerializeversionuid() {
+		return serialVersionUID;
+	}
+	
+
+}
