e4b64c0e87dd80c464eb8cb5326e932ee05b3902
negrimatteo93 <m.negri@itsrizzoli.it>
2019-03-20 14:18:31 +0100

    Res Api

---
+++ rest-api/MyApi/includes/DbOperations.php
@@ -0,0 +1,173 @@
+<?php
+
+   class DbOperations{
+
+     private $con;
+
+     //Costruttore
+     function __construct(){
+
+        require_once dirname(__FILE__) . '/DbConnect.php';
+
+        $db = new DbConnect;
+
+        $this->con = $db->connect();
+
+    }
+
+       //Insert user in database
+       public function createUser($email, $password, $name, $username) {
+
+         if(!$this->isEmailExist($email)){
+           
+              $stmt = $this->con->prepare("INSERT INTO users (email, password, name, username) VALUES (?, ?, ?, ?)");
+              $stmt->bind_param("ssss", $email, $password, $name, $username);
+              if($stmt->execute()){
+
+                  return USER_CREATED;
+
+              }else {
+
+                  return USER_FAILURE;
+
+              }
+           }
+                  return USER_EXISTS;
+
+        }
+
+
+
+        //LoginUser
+       public function userLogin($email, $password){
+
+        if($this->isEmailExist($email)){
+
+          $hashed_password = $this->getUsersPasswordByEmail($email);
+          
+          if(password_verify($password, $hashed_password)){
+
+            return USER_AUTHENTICATED;
+
+           }else {
+
+            return USER_PASSWORD_DO_NOT_MATCH;
+
+           }
+
+        }else{
+
+            return USER_NOT_FOUND;
+
+        }
+
+
+       }
+
+       public function getUsersPasswordByEmail($email){
+
+        $stmt = $this->con->prepare("SELECT password FROM users WHERE email = ?");
+        $stmt-> bind_param("s", $email);
+        $stmt->execute();
+        $stmt->bind_result($password);
+        $stmt->fetch();
+        return $password;
+
+       }
+       
+       //Update user
+       public function updateUser($email, $name, $username, $id){
+
+        $stmt = $this->con->prepare("UPDATE users SET email = ?, name = ?, username = ? WHERE  id = ?");
+        $stmt->bind_param("sssi", $email, $name, $username, $id);
+        if($stmt->execute())
+
+              return true;
+         
+              return false;
+
+       }
+
+       //Update password
+       public function updatePassword($currentpassword, $newpassword, $email){
+
+        $hashed_password = $this->getUsersPasswordByEmail($email);
+
+        if(password_verify($currentpassword, $hashed_password)){
+
+        $hash_password = password_hash($newpassword, PASSWORD_DEFAULT);
+        $stmt = $this->con->prepare("UPDATE users SET password = ? WHERE email = ?");
+        $stmt->bind_param("ss",$hash_password, $email);
+
+        if($stmt->execute())
+        return PASSWORD_CHANGED;
+        return PASSWORD_NOT_CHANGED;
+
+        } else {
+
+           return PASSWORD_DO_NOT_MATCH;
+
+        }
+
+       }
+
+       //Delete user
+       public function deleteUser($id){
+
+        $stmt = $this->con->prepare("DELETE from users WHERE ID = ? ");
+        $stmt->bind_param("i", $id);
+        if($stmt->execute())
+           return true;
+           return false;
+
+       }
+
+       //Selezioni tutti gli utenti
+      public function getAllUsers(){
+
+        $stmt = $this->con->prepare("SELECT id, email, name, username FROM users;");
+        $stmt->execute();
+        $stmt->bind_result($id, $email, $name, $username);
+        $users = array();
+        while($stmt ->fetch()){
+
+          $user = array();
+          $user['id'] = $id;
+          $user['email'] = $email;
+          $user['name'] = $name;
+          $user['username'] = $username;
+          array_push($users, $user);
+        
+        }
+
+        return $users;
+        
+       }
+
+    
+      public function getUserByEmail($email){
+
+        $stmt = $this->con->prepare("SELECT id, email, name, username FROM users WHERE email = ?");
+        $stmt-> bind_param("s", $email);
+        $stmt->execute();
+        $stmt->bind_result($id, $email, $name, $username);
+        $stmt ->fetch();
+        $user['id'] = $id;
+        $user['email'] = $email;
+        $user['name'] = $name;
+        $user['username'] = $username;
+        return $user;
+        
+      }
+
+      private function isEmailExist($email){
+
+         $stmt = $this->con->prepare("SELECT id FROM users WHERE email = ?");
+         $stmt->bind_param("s", $email);
+         $stmt->execute();
+         $stmt->store_result();
+         return $stmt->num_rows > 0;
+
+       }
+
+   }
